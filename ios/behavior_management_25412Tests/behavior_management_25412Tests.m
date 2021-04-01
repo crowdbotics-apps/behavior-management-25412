6221bd620d3990380c55cabf2c68112841d545d2
Crowdbotics <team@crowdbotics.com>
2021-04-01 15:12:34 +0000

    Initial commit.

---
+++ ios/behavior_management_25412Tests/behavior_management_25412Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface behavior_management_25412 : XCTestCase
+
+@end
+
+@implementation behavior_management_25412
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
